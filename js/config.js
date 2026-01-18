9d6a9f8d3181d0e14d82a81ab2e54e0797ac8808
Román Yakimovicz <romanyaki@hotmail.com>
2026-01-18 20:32:07 -0300

    agrega versión en el footer (v0.5.0)

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  version: "v0.5.0", // Major.Minor.Patch
+  fechaUpdate: "2026-01-18",
+  autor: "Román Yakimovicz",
+};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 /* Main Entry Point */
 import "./menu.js";
 import { gameManager } from "./game-manager.js";
+import { CONFIG } from "./config.js";
 
 console.log("Main Loaded. Daily Seed:", gameManager.currentSeed);
+
+// Display Version
+function displayVersion() {
+  const footerP = document.querySelector(".main-footer p");
+  if (footerP) {
+    // Separator (normal size)
+    footerP.appendChild(document.createTextNode(" | "));
+
+    // Version Tag (smaller)
+    const versionSpan = document.createElement("span");
+    versionSpan.className = "version-tag";
+    versionSpan.innerText = CONFIG.version;
+    footerP.appendChild(versionSpan);
+  }
+  console.log(
+    `%c JIGSUDO ${CONFIG.version} cargado correctamente`,
+    "background: #F37825; color: white; padding: 2px 5px; border-radius: 3px;",
+  );
+}
+
+displayVersion();

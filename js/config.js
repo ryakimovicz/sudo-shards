8192d9c16ef5714ca3dfa14d0da4c8d0ba99f4ba
Román Yakimovicz <romanyaki@hotmail.com>
2026-01-26 21:32:30 -0300

    Actualiza a v0.6.1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
-  version: "v0.6.0", // Major.Minor.Patch
+  version: "v0.6.1", // Major.Minor.Patch
   fechaUpdate: "2026-01-27",
   autor: "Román Yakimovicz",
-  debugMode: true, // Toggle to true to see logs and debug panels
+  debugMode: false, // Toggle to true to see logs and debug panels
   betaMode: true, // Toggle to true to enable "Ayuda" button
 };
